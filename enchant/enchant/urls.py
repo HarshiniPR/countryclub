4acbc944eb13726d3078a8b87aaf3550ae5b66cf
vranda25 <202052349@iiitvadodara.ac.in>
2022-04-28 23:26:01 +0530

    front

--- enchant/enchant/urls.py
+++ enchant/enchant/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name='index'),
+    path('login/',views.login, name='login'),
+    path('mr/',views.mr, name='mr'),
+    path('reciprocals/',views.reciprocals, name='reciprocals'),
+    path('signup/',views.signup, name='signup'),
 ]
